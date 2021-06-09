03353f65e6f18183d92a7d042c5967b4331fd83a
Franziska <franziska.niemeyer@fh-bielefeld.de>
2021-06-09 21:27:32 +0200

    - moved custom css from App.vue to app.scss for easier modification and reusability - revised customization of theme map colors - added links in nav bar (faq & profile) - removed 'Save Changes' button in video pop up on HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -26,8 +26,7 @@
 
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
