771a66e9320869160390edc7dd055c0c8412e523
Lars <behethangames@gmail.com>
2021-06-26 19:16:06 +0200

    -- Added login/registration popups ( DO NOT REMOVE MY SHIT AGAIN !!!!!!!! )

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,134 @@
 <template>
   <div>
+    <div id="app">
+      <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+        <div class="container-fluid">
 
-  <Header></Header>
+          <!-- The website icon -->
+          <a class="navbar-brand" href="/">
+            <!--<img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24">-->
+            <img src="./assets/logo_541x500.png" alt="" width="39" height="36">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-    <!-- Content will be here! -->
+          <!-- Left area with home and about pages-->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-  <Footer></Footer>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
+              <li class="nav-item">
+                <router-link class="nav-link active green" aria-current="page" to="/">
+                  <a class="green">Home</a>
+                </router-link>
+              </li>
+
+              <li class="nav-item">
+                <router-link class="nav-link active" aria-current="page" to="/About">
+                  <a class="green">About</a>
+                </router-link>
+              </li>
+            </ul>
+
+            <!-- Right flex area with login and register links -->
+            <div class="d-flex">
+
+              <button class="btn btn-link green text-capitalize" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+              <button class="btn btn-link green text-capitalize" data-bs-toggle="modal" data-bs-target="#testModal">Register</button>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <br>
+
+      <router-view></router-view>
+
+      <!-- Login Modal -->
+      <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-login">
+          <div class="modal-content">
+            <div class="modal-body">
+
+              <div class="container">
+                <h1 class="display-6" style="margin: 10%">Login</h1>
+
+                <div class="row">
+
+                  <form>
+                    <div class="mb-4">
+                      <input type="email" class="form-control" id="loginInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email Adress...">
+                    </div>
+                    <div class="mb-4">
+                      <input type="password" class="form-control" id="registerInputPassword1" placeholder="Enter Password...">
+                    </div>
+                    <div class="mb-4">
+                      <button type="submit" class="btn btn-primary" style="width: 100%">Login</button>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Regiser Modal -->
+      <div class="modal fade" id="testModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-login">
+          <div class="modal-content">
+            <div class="modal-body">
+
+              <div class="container">
+                <h1 class="display-6" style="margin: 10%">Register</h1>
+
+                <div class="row">
+
+                  <form>
+                    <div class="mb-3">
+                      <input type="text" class="form-control" id="firstName" placeholder="First Name">
+                    </div>
+
+                    <div class="mb-3">
+                      <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+                    </div>
+
+                    <div class="mb-3">
+                      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                    </div>
+                    <div class="mb-3">
+                      <input type="password" class="form-control" id="password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary" style="width: 100%">Login</button>
+                  </form>
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br><br><br><br> <br><br><br><br> <br><br><br><br> <br><br><br><br> <br><br><br><br>
+
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+//import HelloWorld from './components/HelloWorld.vue'
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-
 export default {
   name: 'App',
   components: {
-    Header,
     Footer
   }
 }
@@ -33,7 +143,4 @@ export default {
   color: #2c3e50;
   #margin-top: 60px;
 }
-
-
-
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,54 +1,44 @@
 <template>
-<div>
-  <div class="container">
-    <div class="row">
+  <div>
+    <div class="container">
+      <div class="row">
 
-      <div class="col-md-5">
+        <div class="col-md-5">
 
-        <br><br><br><br><br><br>
+          <br><br><br><br><br><br>
 
-        <!-- Eycatcher -->
-        <h1 class="font-monospace">Donate Money with Blockchain</h1>
-        <h5>Simple to use Blockchain Donations</h5>
+          <!-- Eycatcher -->
+          <h1 class="font-monospace">Donate Money with Blockchain</h1>
+          <h5>Simple to use Blockchain Donations</h5>
 
-        <br><br>
+          <br><br>
 
-        <button class="btn btn-primary ms-1 me-3" data-spy="scroll" data-bs-target="#projects">Explore</button>
-        <button class="btn btn-primary ms-1 me-3" data-bs-toggle="modal" data-bs-target="#videoModal">Watch Video</button>
+          <button class="btn btn-primary ms-1 me-3" data-spy="scroll" data-bs-target="#projects">Explore</button>
+          <button class="btn btn-primary ms-1 me-3" data-bs-toggle="modal" data-bs-target="#videoModal">Watch Video</button>
 
-        <!-- Video Modal -->
-        <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content ">
-        <button class="btn btn-primary ms-1 me-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Watch Video</button>
-            </div>
-          </div>
-        </div>
-
-        <!-- Modal -->
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog PopupDialog">
-              <div class="modal-content">
+          <!-- Video Modal -->
+          <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+              <div class="modal-content ">
                 <div class="modal-body">
-                  <div class="embed-responsive embed-responsive-16by9 Video">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" id="video"  allowscriptaccess="always" allow="autoplay" allowfullscreen="always"
-                            style="width:100%; height:100%; border:none;">></iframe>
+
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
                   </div>
+
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-      <div class="col-md-7">
-        <img src="./../assets/homeWithout.png" class="img-fluid" alt="..." height="1280" width="1920">
+        <div class="col-md-7">
+          <img src="./../assets/homeWithout.png" class="img-fluid" alt="..." height="1280" width="1920">
+        </div>
       </div>
     </div>
-  </div>
 
-
-  <section id="explore"></section>
-  <br><br><br><br>
+    <br><br><br><br>
 
 
     <div class="container" id="projects">
@@ -70,25 +60,20 @@
         <div class="col-md-4"> <HomeCard></HomeCard> </div>
       </div>
     </div>
-  </div>
 
+  </div>
 </template>
 
 
 
 <script>
-
 import HomeCard from "@/components/HomeCard";
 export default {
   components: {HomeCard},
   created(){
-
   }
-
 }
-
 </script>
 
 <style scoped>
-
 </style>
