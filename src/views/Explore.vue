46d58a95c2bf1bf3b84db022d20deff699124b7b
fortm <fortmannpaul@gmail.com>
2021-07-24 13:57:16 +0200

    Explore Section added with links

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,45 +1,18 @@
 <template>
+
   <div>
+
     <div class="container">
       <div class="row">
 
-        <div class="col-md-5">
-
-          <br><br><br><br><br><br>
-
-          <!-- Eycatcher -->
-          <h1 class="font-monospace">Donate Money with Blockchain</h1>
-          <h5>Simple to use Blockchain Donations</h5>
+        <div class="col-md-12">
 
           <br><br>
 
-          <button class="btn btn-primary ms-1 me-3" data-spy="scroll" @click="$router.push({name : 'Explore' })">Explore</button>
-          <button class="btn btn-primary ms-1 me-3" data-bs-toggle="modal" data-bs-target="#videoModal">Watch Video
-          </button>
-
-          <!-- Video Modal -->
-          <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered">
-              <div class="modal-content ">
-                <div class="modal-body">
-
-                  <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" id="video"
-                            allowscriptaccess="always" allow="autoplay"></iframe>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-7">
-          <img src="./../assets/homeWithout.png" class="img-fluid" alt="..." height="1280" width="1920">
+          <h1 class="font-monospace">Explore All Projects</h1>
         </div>
       </div>
-    </div>
-
+      </div>
     <br><br><br><br>
 
 
@@ -107,38 +80,46 @@
           <HomeCard></HomeCard>
         </div>
       </div>
+
+
+      <div class="row">
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+        <div class="col-md-4">
+          <HomeCard></HomeCard>
+        </div>
+      </div>
+
     </div>
 
   </div>
-</template>
 
+</template>
 
 <script>
 import HomeCard from "@/components/HomeCard";
-import Vue from 'vue'
-
 export default {
   components: {HomeCard},
-  data() {
-    return {
-      projects: [],
-      project: ''
-    }
-  },
-  created: function () {
-    const baseURI = 'projects'
-    Vue.axios.get(baseURI, {
-
-    }).then((response) => {
-      console.log(response.data)
-      this.projects = response.data;
-
-    }).catch(err => {
-      console.log(err.response);
-    });
-  },
+  name: "Explore"
 }
 </script>
 
 <style scoped>
+
 </style>
