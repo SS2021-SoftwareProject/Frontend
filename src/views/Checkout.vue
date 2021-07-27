4a695e88b9bd6d64921e6dba99005a4127db165c
fortm <fortmannpaul@gmail.com>
2021-07-27 16:38:59 +0200

    Push für Lars

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -151,7 +151,6 @@
 
 <script>
 
-import $ from '../store/EtherValue'
 var http = require('http');
 
 export default {
@@ -161,14 +160,24 @@ export default {
       document.getElementById("emailForm").submit();
       document.forms["emailForm"].submit();
     },
-  curl : function (){
-    window.alert("test")
 
 
+  getRate : function (from, to){
+    //window.alert("test")
 
+    var script = document.createElement('script');
+    script.setAttribute('src', "http://query.yahooapis.com/v1/public/yql?q=select%20rate%2Cname%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes%3Fs%3D"+from+to+"%253DX%26f%3Dl1n'%20and%20columns%3D'rate%2Cname'&format=json&callback=parseExchangeRate");
 
+    window.alert(script.innerHTML)
+    document.body.appendChild(script);
+  },
 
+  parseExchangeRate(data){
+    var name = data.query.results.row.name;
+    var rate = parseFloat(data.query.results.row.rate, 10);
+    window.alert("Exchange rate " + name + " is " + rate);
   }
+
 
   },
   data(){
@@ -177,7 +186,8 @@ export default {
     }
   },
   created(){
-    this.curl()
+    this.getRate("SEK", "USD");
+    this.getRate("USD", "SEK");
   }
 }
 
