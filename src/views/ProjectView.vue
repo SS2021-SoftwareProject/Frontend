b956fa71deab78573b1bab0247895247fc25bdd0
fortm <fortmannpaul@gmail.com>
2021-08-03 20:50:45 +0200

    Images fetched from real server now, issue #76

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -219,7 +219,7 @@ export default {
 
     //Get Overview data
 
-    Vue.axios.get('projects/description/'+projectID+1, {}).then((response) => {
+    Vue.axios.get('projects/descriptions/'+projectID+1, {}).then((response) => {
 
       this.imgage1 = response.data.imageSum
       this.image2 = response.data.imageSol
